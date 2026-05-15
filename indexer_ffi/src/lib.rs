64c9259958dc2fa213702a257aafb5f5622fce0c
jonesmarvin8 <83104039+jonesmarvin8@users.noreply.github.com>
2026-05-15 10:01:53 -0400

    fix artifacts

--- indexer_ffi/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(clippy::undocumented_unsafe_blocks, reason = "It is an FFI")]
-
-pub use errors::OperationStatus;
-pub use indexer::IndexerServiceFFI;
-
-pub mod api;
-mod errors;
-mod indexer;
