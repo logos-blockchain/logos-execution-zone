logos-blockchain/lssa

fixed lint and unit test

nssa/src/signature/private_key.rs
@@ -47,13 +47,11 @@ impl PrivateKey {
         bytes.extend_from_slice(&secp256k1::PublicKey::serialize(&pk));
         let hashed: [u8; 32] = Impl::hash_bytes(&bytes).as_bytes().try_into().unwrap();
 
-        let tweaked_sk = PrivateKey::try_new(
+        PrivateKey::try_new(
             sk.add_tweak(&secp256k1::Scalar::from_be_bytes(hashed).unwrap())
                 .expect("Expect a valid Scalar")
                 .secret_bytes(),
-        );
-
-        tweaked_sk
+        )
     }
 }
 

logos-blockchain/logos-execution-zone

lee/state_machine/src/signature/private_key.rs
@@ -73,15 +73,13 @@ impl PrivateKey {
         bytes.extend_from_slice(pk.to_encoded_point(true).as_bytes());
         let hashed: [u8; 32] = Sha256::digest(&bytes).into();
 
-        let tweaked_sk = Self::try_new(
+        Self::try_new(
             k256::Scalar::from_repr((*value).into())
                 .unwrap()
                 .add(&k256::Scalar::from_repr(hashed.into()).unwrap())
                 .to_bytes()
                 .into(),
-        );
-
-        tweaked_sk
+        )
     }
 }
 