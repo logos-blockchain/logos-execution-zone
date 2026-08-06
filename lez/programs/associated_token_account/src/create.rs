05bc4ac2ef299148ecf7161e7415ed17ab5e9de7
Artem Gureev <artguril@proton.me>
2026-08-06 17:10:04 +0000

    fix(lez): stop claiming owner key accounts in ATA create

--- lez/programs/associated_token_account/src/create.rs
+++ lez/programs/associated_token_account/src/create.rs
@@ -1,6 +1,6 @@
 use lee_core::{
     account::{Account, AccountWithMetadata},
-    program::{AccountPostState, ChainedCall, Claim, ProgramId},
+    program::{AccountPostState, ChainedCall, ProgramId},
 };
 
 pub fn create_associated_token_account(
@@ -22,7 +22,7 @@ pub fn create_associated_token_account(
     if ata_account.account != Account::default() {
         return (
             vec![
-                AccountPostState::new_claimed_if_default(owner.account.clone(), Claim::Authorized),
+                AccountPostState::new(owner.account.clone()),
                 AccountPostState::new(token_definition.account.clone()),
                 AccountPostState::new(ata_account.account.clone()),
             ],
@@ -31,7 +31,7 @@ pub fn create_associated_token_account(
     }
 
     let post_states = vec![
-        AccountPostState::new_claimed_if_default(owner.account.clone(), Claim::Authorized),
+        AccountPostState::new(owner.account.clone()),
         AccountPostState::new(token_definition.account.clone()),
         AccountPostState::new(ata_account.account.clone()),
     ];
